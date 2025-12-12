b95bc6305b2f3a414b49819df575dbdf70d3dfc8
BenenB <benenbruen@gmail.com>
2025-12-12 12:20:38 +0000

    multi-sequence runner

--- morse_code/main.py
+++ morse_code/main.py
@@ -1,6 +1,6 @@
 from machine import Pin
-
-from morse_code import MorseGenerator
+import time
+from morse_code import MorseGenerator,MultiSequenceRunner
 
 def set_pin(pin):
     pin.value(1)
@@ -11,11 +11,34 @@ def unset_pin(pin):
 if __name__ == "__main__":
 
     onboardLED = Pin(25, Pin.OUT)
+    yellow = Pin(18, Pin.OUT)
+    green = Pin(19, Pin.OUT)
+    red = Pin(20, Pin.OUT)
 
-    generator = MorseGenerator(bps=4)
-    sequence = generator.generate("Hello world!")
-
-    sequence.run(
-        set=(lambda: set_pin(onboardLED)),
-        unset=(lambda: unset_pin(onboardLED))
-    )
+    generator = MorseGenerator()
+    
+    red_seq = generator.generate("Hello world from RED")
+    red_seq.define_set_function((lambda: set_pin(red)))
+    red_seq.define_unset_function((lambda: unset_pin(red)))
+    red_seq.loop = False
+    
+    green_seq = generator.generate("GREEN says Hello world")
+    green_seq.define_set_function((lambda: set_pin(green)))
+    green_seq.define_unset_function((lambda: unset_pin(green)))
+    green_seq.loop = False
+    
+    yellow_seq = generator.generate("Deireann BUI dia duit ar domhan")
+    yellow_seq.define_set_function((lambda: set_pin(yellow)))
+    yellow_seq.define_unset_function((lambda: unset_pin(yellow)))
+    yellow_seq.loop = False
+    
+    runner = MultiSequenceRunner(bps=8)
+    runner.add_sequence(red_seq)
+    runner.add_sequence(green_seq)
+    runner.add_sequence(yellow_seq)
+    
+    runner.run_sequences()
+    
+    print("Done")
+    
+    
--- morse_code/morse_code.py
+++ morse_code/morse_code.py
@@ -29,15 +29,43 @@ class BadSequenceException(Exception):
         self.msg = message
 
     def __str__(self):
-        return f"UnsupportedSequence: {self.msg}"
+        return f"BadSequence: {self.msg}"
+    
+class NoFunctionDefinedException(Exception):
+    def __init__(self, message: str = ""):
+        self.msg = message
+
+    def __str__(self):
+        return f"NoFunctionDefined: {self.msg}"
 
     
 class MorseSequence:
-    def __init__(self, sequence: str, unit_length: float = 0.5):
+    def __init__(self, sequence: str, unit_length: float = 0.5, set: function = None, unset: function = None, loop: bool = False):
         self.sequence = sequence
         self.unit_length = unit_length
+        self.__generate_step_sequence()
+        self.sequence_length = len(self.step_sequence)
+        self.set_function = set
+        self.unset_function = unset
+        self.loop = loop
 
-    def run(self, set: function, unset: function, always_unset: bool = False):        
+    def define_set_function(self, fn: function):
+        self.set_function = fn
+
+    def define_unset_function(self, fn: function):
+        self.unset_function = fn
+     
+    def run(self, set: function = None, unset: function = None, always_unset: bool = False):
+        if not set:
+            if not self.set_function:
+                raise NoFunctionDefinedException("set not defined")
+            set = self.set_function
+
+        if not unset:
+            if not self.unset_function:
+                raise NoFunctionDefinedException("unset not defined")
+            unset = self.unset_function
+        
         if not self.sequence:
             return
         
@@ -61,12 +89,78 @@ class MorseSequence:
             elif always_unset:
                 unset()
 
-    def print(self):
-        print(str(self))
+    def run_step(self, step: int, set: function = None, unset: function = None):
+        if not set:
+            if not self.set_function:
+                raise NoFunctionDefinedException("set not defined")
+            set = self.set_function
+
+        if not unset:
+            if not self.unset_function:
+                raise NoFunctionDefinedException("unset not defined")
+            unset = self.unset_function
+
+        if step > self.sequence_length:
+            if self.loop:
+                step = step % self.sequence_length
+            else:
+                return True
+        
+        instruction = self.step_sequence[step]
+
+        if instruction == "up":
+            set()
+        if instruction == "down":
+            unset()
+
+        return False
+
+
+    def __generate_step_sequence(self):
+        steps = []
+        for char in list(self.sequence):
+            if char == ".":
+                steps.extend(["up","down"])
+            if char == "-":
+                steps.extend(["up","wait","wait","down"])
+            if char == "_":
+                steps.extend(["wait"] * 4)
+            if char == "|":
+                steps.extend(["wait"] * 8)
+
+        self.step_sequence = steps
 
     def __str__(self) -> str:
         return self.sequence.replace("_"," ").replace("|","   ")
+    
+    def print(self):
+        print(str(self))
+    
 
+class MultiSequenceRunner:
+    def __init__(self, bps: float = 3):
+        self.sequences = []
+        if bps <= 0:
+            raise ValueError(f"Generator bps must be greater than 0, recieved {bps}")
+        self.unit_length = 1/bps
+
+    def add_sequence(self, sequence: MorseSequence):
+        self.sequences.append(sequence)
+
+    def run_sequences(self):
+        current_sequences = self.sequences.copy()
+        current_step = 0
+        
+        while len(current_sequences):
+            seqs_to_remove = []
+            for i,seq in enumerate(current_sequences):
+                if seq.run_step(current_step):
+                    seqs_to_remove.append(i)
+            
+            if seqs_to_remove:
+                current_sequences = [seq for i,seq in enumerate(current_sequences) if not i in seqs_to_remove ]
+
+            current_step += 1
 
 class MorseGenerator:
     def __init__(self, bps: float = 3):
@@ -90,7 +184,6 @@ class MorseGenerator:
         sequence = [ self.__convert_char(char) for char in char_array ]
         sequence = [ char for char in sequence if char ]
 
-        
         return "_".join(sequence)
     
     def __convert_char(self, char: str, ignore_unsupported: bool = True) -> str:
